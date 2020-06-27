fd0855172aaffbff9ba23db51412d6047e8bd837
hashan <hashan.20@cse.mrt.ac.lk>
2020-06-28 03:32:29 +0530

    fusion-1 : bootstrap client impel

--- distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
+++ distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
@@ -1,13 +1,21 @@
 package lk.uom.cse.fusion.distributedcontentsearchingnode;
 
+import lk.uom.cse.fusion.distributedcontentsearchingnode.comms.BSClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DistributedContentSearchingNodeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DistributedContentSearchingNodeApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DistributedContentSearchingNodeApplication.class, args);
+  }
 
+  @Bean
+  public BSClient modelMapper() throws IOException {
+    return new BSClient();
+  }
 }
