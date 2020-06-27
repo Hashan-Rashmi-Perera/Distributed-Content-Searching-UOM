994e2939147e6026aa479d8d6bb23c2cd2039edc
hashan <hashan.20@cse.mrt.ac.lk>
2020-06-28 02:34:57 +0530

    fusion-1 : initial commit

---
+++ distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
@@ -0,0 +1,13 @@
+package lk.uom.cse.fusion.distributedcontentsearchingnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedContentSearchingNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedContentSearchingNodeApplication.class, args);
+    }
+
+}
