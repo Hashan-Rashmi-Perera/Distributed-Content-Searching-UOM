c5348f1ae63d3e6c68f967ea5556cdb527589add
hashan <hashan.20@cse.mrt.ac.lk>
2020-07-04 11:41:00 +0530

    fusion-1 : append sample project impel

--- distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
+++ distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
@@ -1,12 +1,13 @@
 package lk.uom.cse.fusion.distributedcontentsearchingnode;
 
 import lk.uom.cse.fusion.distributedcontentsearchingnode.comms.BSClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
-
+@Slf4j
 @SpringBootApplication
 public class DistributedContentSearchingNodeApplication {
 
@@ -17,8 +18,13 @@ public class DistributedContentSearchingNodeApplication {
   @Bean
   public BSClient bsClient() throws IOException {
     BSClient bsClient = new BSClient();
-    //bsClient.unRegister("hashan", "localhost", 8081);
-    bsClient.register("hashan", "localhost", 8081);
-    return bsClient;
+    try {
+      // bsClient.unRegister("hashan", "localhost", 8081);
+      bsClient.register("hashan", "localhost", 8081);
+    } catch (Exception e) {
+      log.error("Error registering with bootstrap server: ", e);
+    } finally {
+      return bsClient;
+    }
   }
 }
---
+++ distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/comms/P2PMessage.java
@@ -0,0 +1,4 @@
+package lk.uom.cse.fusion.distributedcontentsearchingnode.comms;
+
+public interface P2PMessage {
+}
