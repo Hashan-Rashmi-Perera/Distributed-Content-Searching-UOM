0b444ec48e126e3322e71b200bea3b25bf464dae
osura <odgamalath@gmail.com>
2020-07-11 12:38:31 +0530

    bs client with arguements added.

--- distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
+++ distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/DistributedContentSearchingNodeApplication.java
@@ -1,24 +1,42 @@
 package lk.uom.cse.fusion.distributedcontentsearchingnode;
 
 import lk.uom.cse.fusion.distributedcontentsearchingnode.comms.BSClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import java.io.IOException;
 
 @SpringBootApplication
 public class DistributedContentSearchingNodeApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(DistributedContentSearchingNodeApplication.class, args);
-  }
+     public static BSClient bsClient;
 
-  @Bean
-  public BSClient bsClient() throws IOException {
-    BSClient bsClient = new BSClient();
-    //bsClient.unRegister("hashan", "localhost", 8081);
-    bsClient.register("hashan", "localhost", 8081);
-    return bsClient;
-  }
+    public static void main(String[] args) {
+    SpringApplication.run(DistributedContentSearchingNodeApplication.class, args);
+
+
+        try {
+
+            bsClient = new BSClient();
+
+
+            if(args.length>0)  bsClient.register(args[0],args[1],Integer.parseInt(args[2]));
+            else {
+                bsClient.register("osura", "localhost", 8081);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+//  @Bean
+//  public BSClient bsClient() throws IOException {
+//    BSClient bsClient = new BSClient();
+//    //bsClient.unRegister("hashan", "localhost", 8081);
+//
+//    return bsClient;
+//  }
 }
