5011cf601685e9d6d1bc0db96548cfb02dcb68de
hashan <hashan.20@cse.mrt.ac.lk>
2020-07-27 00:02:11 +0530

    quarry param update

--- distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/resource/SearchController.java
+++ distributed-content-searching-node/src/main/java/lk/uom/cse/fusion/distributedcontentsearchingnode/resource/SearchController.java
@@ -3,6 +3,7 @@ package lk.uom.cse.fusion.distributedcontentsearchingnode.resource;
 
 import lk.uom.cse.fusion.distributedcontentsearchingnode.core.SearchResult;
 import lk.uom.cse.fusion.distributedcontentsearchingnode.service.SearchService;
+import lk.uom.cse.fusion.distributedcontentsearchingnode.utils.ConsoleTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
@@ -15,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping(value = "client")
@@ -26,20 +30,21 @@ public class SearchController {
 
 
     @GetMapping(value="search")
-    public Map<String, SearchResult> searchForAFile(@RequestParam("key") String fileName){
-        return searchService.searchFile(fileName);
+    public Map<Integer, SearchResult> searchForAFile(@RequestParam("key") String fileName){
+        Map<String, SearchResult> searchResultMap = searchService.searchFile(fileName);
+        Map<Integer, SearchResult> resultMap = new HashMap<>();
+        AtomicInteger index = new AtomicInteger(1);
+        searchResultMap.forEach((key, value) -> resultMap.put(index.getAndIncrement(), value));
+        return resultMap;
     }
 
     @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity downloadFileWithGet(@RequestParam("file") Integer fileOption) throws IOException {
+    public ResponseEntity downloadFileWithGet(@RequestParam("search-result-id") Integer fileOption) throws IOException {
         File downloadFile = searchService.downloadFile(fileOption);
         InputStreamResource resource = new InputStreamResource(new FileInputStream(downloadFile));
         return ResponseEntity.ok()
                 .header("Content-Disposition", "attachment;filename=" + downloadFile.getName())
                 .contentLength(downloadFile.length())
                 .body(resource);
-        //Response.ResponseBuilder response = Response.ok(fileDownload);
-        //response.header("Content-Disposition", "attachment;filename=" + fileDownload.getName());
-        //return response.build();
     }
 }
