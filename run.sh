058e44c23ed9f2b53db2a6b91bcd138d412846d1
hashan <hashan.20@cse.mrt.ac.lk>
2020-08-01 15:38:00 +0530

    run.sh java heap

--- run.sh
+++ run.sh
@@ -3,11 +3,10 @@ n=$1
 mvn clean install -DskipTests;
 path="distributed-content-searching-node/target/*.jar"
 port=8080
-konsole --new-tab -e java -cp BootstrapServer/Java/: BootstrapServer &
+konsole --new-tab -e java -Xms10M -Xmx10M -cp BootstrapServer/Java/: BootstrapServer &
 for (( i = 1; i <= $n; i++ )); do
     echo $path;
     p=`expr $port + $i`;
-    konsole --new-tab -e java -jar $path --server.port=$p &
+    konsole --new-tab -e java -Xms20M -Xmx20M -jar $path --server.port=$p &
     sleep 10
-    #konsole java -jar $path --server.port=$p .;
 done
